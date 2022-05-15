26b2bd842fed62457ddb190e09ed40a2729222b9
Thomas Phillimore <tom.phillimore@googlemail.com>
2022-05-15 13:02:08 +0100

    Created Fund model

--- apps/funds/models.py
+++ apps/funds/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+STRATEGY_CHOICES = (
+    ('equity', 'Long/Short Equity'),
+    ('global', 'Global Macro'),
+    ('arbitrage', 'Arbitrage'),
+)
+
+class FundManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+class Fund(models.Model):
+    objects = FundManager()
+    
+    name = models.CharField(
+        max_length=100
+    )
+
+    strategy = models.CharField(
+        max_length=100,
+        choices=STRATEGY_CHOICES,
+    )
+
+    aum = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+
+    inception_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    is_deleted = models.BooleanField(
+        default=False,
+    )
+
+    def __str__(self):
+        return self.name
