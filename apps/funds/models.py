bd2a8d5abe9e256e1947480a667f786aaa351604
Thomas Phillimore <tom.phillimore@googlemail.com>
2022-05-15 15:02:26 +0100

    Created FundCSV models / migrations

--- apps/funds/models.py
+++ apps/funds/models.py
@@ -1,4 +1,10 @@
+import csv
+import io
+
 from django.db import models
+from django.core.exceptions import ValidationError
+
+from .validators import FundCSVRow
 
 STRATEGY_CHOICES = (
     ('equity', 'Long/Short Equity'),
@@ -6,9 +12,25 @@ STRATEGY_CHOICES = (
     ('arbitrage', 'Arbitrage'),
 )
 
+CSV_STATUS_CHOICES = (
+    ('pending', 'Pending'),
+    ('processing', 'Processing'),
+    ('processed', 'Processed'),
+    ('error', 'Error'),
+)
+
+CSV_FIELD_NAMES = {
+    'Name': 'name',
+    'Strategy': 'strategy',
+    'AUM (USD)': 'aum',
+    'Inception Date': 'inception_date',
+}
+
+
 class FundManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_deleted=False)
+
 
 class Fund(models.Model):
     objects = FundManager()
@@ -38,3 +60,18 @@ class Fund(models.Model):
 
     def __str__(self):
         return self.name
+
+    
+class FundCSV(models.Model):
+    file = models.FileField(
+        upload_to='uploads/csv/',
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    status = models.CharField(
+        max_length=100,
+        default='pending',
+    )
